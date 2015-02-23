24b4861903b3c841746a8fae6b4970230f8fe8d5
Ryan Bateman <ryanbateman.uw@gmail.com>
2015-02-22 21:56:54 -0500

    Added basic map structure
    If you have any trouble merging the GameScene, just take your changes
    and drop the Map prefab into the scene. It contains everything this
    commit changed.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Map : MonoBehaviour {
+
+	public static Map instance = null;
+
+	List<Region> regions = new List<Region>();
+
+	// Use this for initialization
+	void Awake () {
+		Map.instance = this;
+		this.regions.AddRange(this.GetComponentsInChildren<Region>());
+	}
+
+	void OnDestroy() {
+		Map.instance = null;
+	}
+}
---
+++ Assets/Scripts/Region.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Region : MonoBehaviour {
+	
+	public List<Region> neighbors = new List<Region>();
+
+	public Image image;
+
+	public int wood = 0;
+
+	public int stone = 0;
+
+	public int food = 0;
+
+	void Awake() {
+		this.wood = Random.Range(0, 10);
+		this.stone = Random.Range(0, 10);
+		this.food = Random.Range(0, 10);
+
+		this.image = this.GetComponentInChildren<Image>();
+
+		Button button = this.gameObject.AddComponent<Button>() as Button;
+
+		EventTrigger trigger = this.gameObject.AddComponent<EventTrigger>() as EventTrigger;
+		EventTrigger.Entry entry = new EventTrigger.Entry();
+		entry.eventID = EventTriggerType.PointerEnter;
+		entry.callback = new EventTrigger.TriggerEvent();
+		entry.callback.AddListener(new UnityEngine.Events.UnityAction<BaseEventData>(this.OnPointerEnter));
+		trigger.delegates = new List<EventTrigger.Entry>();
+		trigger.delegates.Add(entry);
+
+		trigger = this.gameObject.AddComponent<EventTrigger>() as EventTrigger;
+		entry = new EventTrigger.Entry();
+		entry.eventID = EventTriggerType.PointerExit;
+		entry.callback = new EventTrigger.TriggerEvent();
+		entry.callback.AddListener(new UnityEngine.Events.UnityAction<BaseEventData>(this.OnPointerExit));
+		trigger.delegates = new List<EventTrigger.Entry>();
+		trigger.delegates.Add(entry);
+	}
+
+	public void OnPointerEnter(BaseEventData baseEvent) {
+		this.image.color = Color.green;
+		foreach (Region r in this.neighbors) {
+			r.image.color = Color.yellow;
+		}
+	}
+
+	public void OnPointerExit(BaseEventData baseEvent) {
+		this.image.color = Color.white;
+		foreach (Region r in this.neighbors) {
+			r.image.color = Color.white;
+		}
+	}
+}
